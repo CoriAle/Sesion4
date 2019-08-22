a00ef4779975d93e027f2ec3336083aa9c1b82a2
CoriN1 <1995coral@hotmail.es>
2019-08-21 21:01:37 -0600

    Antes de migraciones

--- backend/admin.py
+++ backend/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(Producto)
+#admin.site.register(Producto)
--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,7 +1,7 @@
 from .multi_table import *
-from .proxy import *
+"""from .proxy import *
 from .abstract import *
 from .soft_delete_model import SoftDeletedModel
 from .created_updated import *
 from .profesor import *
-from .persona import Persona, Docente, Estudiante
+from .persona import Persona, Docente, Estudiante"""
